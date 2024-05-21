3ac8560a65fdd53595482d91d7677a21cd7ec2e0
leve68 <hanstom000608@gmail.com>
2024-05-21 20:18:17 +0900

    refact: 페이지 라우팅 전역 관리

--- lib/routes.dart
+++ lib/routes.dart
@@ -4,6 +4,46 @@ import 'package:canvas_diary/screens/report/report_screen.dart';
 import 'package:canvas_diary/screens/search/search_screen.dart';
 import 'package:flutter/material.dart';
 
+/// NavItem은 아이콘, label을 담고 있는 모델 클래스로써
+/// BottomNavigationBarItem에 필요한 정보를 제공합니다
+class NavItem {
+  final IconData activeIcon;
+  final String label;
+  final Widget widget;
+
+  const NavItem({
+    required this.activeIcon,
+    required this.label,
+    required this.widget,
+  });
+}
+
+/// 여기서는 그냥 전역적으로 선언해주었습니다
+/// [_navItems]는 [_TabViewState]의 지역 변수로 관리해주어도 되고, provider에 넣어도 되지만
+/// 여기선 그냥 제일 간단하게 전역에서 관리할 예정입니다
+const _navItems = [
+  NavItem(
+    activeIcon: Icons.home,
+    label: 'home',
+    widget: HomeScreen(),
+  ),
+  NavItem(
+    activeIcon: Icons.text_snippet,
+    label: 'report',
+    widget: ReportScreen(),
+  ),
+  NavItem(
+    activeIcon: Icons.search,
+    label: 'search',
+    widget: SearchScreen(),
+  ),
+  NavItem(
+    activeIcon: Icons.person,
+    label: 'mypage',
+    widget: MyPageScreen(),
+  ),
+];
+
 class Routes extends StatefulWidget {
   const Routes({super.key});
 
@@ -27,7 +67,7 @@ class _RoutesState extends State<Routes> with SingleTickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 4, vsync: this);
+    _tabController = TabController(length: _navItems.length, vsync: this);
     _tabController.addListener(tabListener);
   }
 
@@ -40,35 +80,39 @@ class _RoutesState extends State<Routes> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(_navItems[_selectedIndex].label),
+      ),
       body: SafeArea(
         child: TabBarView(
           physics: const NeverScrollableScrollPhysics(), // 스와이핑으로 화면 전환 방지
           controller:
               _tabController, // BottomNavigationBar의 controller와 동일한 컨트롤러 사용
-          children: const [
-            HomeScreen(),
-            ReportScreen(),
-            SearchScreen(),
-            MyPageScreen(),
-          ],
+          children: _navItems.map(
+            (item) {
+              return item.widget;
+            },
+          ).toList(),
         ),
       ),
       bottomNavigationBar: Theme(
         data: ThemeData(
-          bottomNavigationBarTheme: BottomNavigationBarThemeData(),
+          bottomNavigationBarTheme: BottomNavigationBarThemeData(
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: Colors.blueAccent,
+            showSelectedLabels: true,
+            showUnselectedLabels: false,
+          ),
         ),
         child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          selectedItemColor: Colors.blueAccent,
-          showSelectedLabels: true,
-          showUnselectedLabels: false,
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.text_snippet), label: "report"),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: "search"),
-            BottomNavigationBarItem(icon: Icon(Icons.people), label: "my page"),
-          ],
+          items: _navItems.map(
+            (item) {
+              return BottomNavigationBarItem(
+                icon: Icon(item.activeIcon),
+                label: item.label,
+              );
+            },
+          ).toList(),
           currentIndex: _selectedIndex,
           onTap: _onItemTapped,
         ),
