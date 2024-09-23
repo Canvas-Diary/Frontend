d83f7715c55e8695e3d119fa2f21b80aa3488ce4
leve68 <hanstom000608@gmail.com>
2024-09-23 20:38:01 +0900

    fix: 버튼 스타일 중복 수정

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 const Button = ({ size, active, text, onClickHandler }: ButtonProps) => {
   return (
     <button
-      className={`${size === "big" ? "w-[20.4375rem]" : "w-[9.6875rem]"} ${active ? "bg-primary-light-2 text-primary-normal dark:bg-primary-normal dark:text-white" : "bg-primary-light-1 text-primary-light-3"} rounded-200 bg-primary-light-2 py-600 font-Binggrae text-body-1 font-regular text-primary-normal`}
+      className={`${size === "big" ? "w-[20.4375rem]" : "w-[9.6875rem]"} ${active ? "bg-primary-light-2 text-primary-normal dark:bg-primary-normal dark:text-white" : "bg-primary-light-1 text-primary-light-3"} rounded-200 py-600 font-Binggrae text-body-1 font-regular`}
       onClick={onClickHandler}
     >
       {text}
