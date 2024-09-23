980cfc72f909fdf063c03bd33d184ffe68b744f2
leve68 <hanstom000608@gmail.com>
2024-09-24 00:16:13 +0900

    feat: fallback page 생성

---
+++ src/components/layout/fallback/GlobalFallback.tsx
@@ -0,0 +1,5 @@
+const GlobalFallback = () => {
+  return <div>앱 로딩중...</div>;
+};
+
+export default GlobalFallback;
---
+++ src/components/layout/fallback/PageFallback.tsx
@@ -0,0 +1,5 @@
+const PageFallback = () => {
+  return <div>페이지 로딩중...</div>;
+};
+
+export default PageFallback;
