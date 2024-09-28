c69a7204c579a12a1aee423e687ad1030ad455c1
leve68 <hanstom000608@gmail.com>
2024-09-28 21:03:09 +0900

    refactor: App bar 페이지에서 제거하고 layout으로 이동

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../common/Navbar";
+import Appbar from "../common/Appbar";
 
 const Layout = () => {
+  const location = useLocation();
+
+  const shouldDeleteAppBar = () => {
+    const pathsWithAppBar = ["/", "/album"];
+    return pathsWithAppBar.includes(location.pathname);
+  };
+
+  const getAppBarText = () => {
+    switch (location.pathname) {
+      case "/explore":
+        return "explore";
+      case "/mypage":
+        return "마이페이지";
+      default:
+        return "";
+    }
+  };
+
   return (
     <div className="flex h-screen w-screen flex-col">
+      {!shouldDeleteAppBar() && (
+        <Appbar text={getAppBarText()} backHandler={() => {}} menuHandler={() => {}} />
+      )}
       <div className="flex-grow overflow-scroll">
         <Outlet />
       </div>
