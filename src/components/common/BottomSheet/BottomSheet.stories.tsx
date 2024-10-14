e8949b9c01cdc38c4c7b13fb2cea2906dc76b2d6
leve68 <hanstom000608@gmail.com>
2024-10-15 01:47:23 +0900

    feat: 바텀 시트 레이아웃

---
+++ src/components/common/BottomSheet/BottomSheet.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import BottomSheet from "./BottomSheet";
+
+const meta: Meta<typeof BottomSheet> = {
+  component: BottomSheet,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof BottomSheet>;
+
+export const Primary: Story = {
+  args: {},
+};
