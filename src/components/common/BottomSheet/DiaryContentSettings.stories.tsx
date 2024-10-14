b3414c0cdaa2ee4afe5ebec67261a127835070b0
leve68 <hanstom000608@gmail.com>
2024-10-15 01:47:58 +0900

    feat: 일기 설정 모달 레이아웃

---
+++ src/components/common/BottomSheet/DiaryContentSettings.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import DiaryContentSettings from "./DiaryContentSettings";
+
+const meta: Meta<typeof DiaryContentSettings> = {
+  component: DiaryContentSettings,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof DiaryContentSettings>;
+
+export const Primary: Story = {
+  args: {},
+};
