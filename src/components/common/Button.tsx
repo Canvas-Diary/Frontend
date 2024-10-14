85968c8c018005b33644f825ee4657a803c16b7d
leve68 <hanstom000608@gmail.com>
2024-10-15 01:49:59 +0900

    feat: gray 버튼에 대응하기 위해 Button Props 수정

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,21 +2,30 @@ interface ButtonProps {
   size: "big" | "small";
   active: boolean;
   text: string;
+  bgColor: "light" | "dark" | "gray";
   onClickHandler: () => void;
 }
+
+const bgColorStyle = {
+  light: "bg-primary-light-2 text-primary-normal",
+  dark: "bg-primary-normal text-white",
+  gray: "bg-gray-100 text-gray-500",
+  non: "bg-primary-light-1 text-primary-light-3",
+};
 
 /**
  * 공통 Button
  * @param size  버튼 크기
  * @param active 활성화 상태
  * @param text 버튼 글귀
+ * @param bgColor 배경 색상
  * @param onClickHandler 버튼 클릭 콜백 함수
  * @returns
  */
-const Button = ({ size, active, text, onClickHandler }: ButtonProps) => {
+const Button = ({ size, active, text, bgColor, onClickHandler }: ButtonProps) => {
   return (
     <button
-      className={`${size === "big" ? "w-[20.4375rem]" : "w-[9.6875rem]"} ${active ? "bg-primary-light-2 text-primary-normal dark:bg-primary-normal dark:text-white" : "bg-primary-light-1 text-primary-light-3"} rounded-200 py-600 font-Binggrae text-body-1 font-regular`}
+      className={`${size === "big" ? "w-[20.4375rem]" : "w-[9.6875rem]"} ${active ? bgColorStyle[bgColor] : bgColorStyle.non} rounded-200 py-600 font-Binggrae text-body-1 font-regular`}
       onClick={onClickHandler}
     >
       {text}
