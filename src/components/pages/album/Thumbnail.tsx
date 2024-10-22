0c49a171cbed8597f5af86ac7f73bf7f7c648077
leve68 <hanstom000608@gmail.com>
2024-10-22 19:03:59 +0900

    fix: img 보이도록 수정

--- src/components/pages/album/Thumbnail.tsx
+++ src/components/pages/album/Thumbnail.tsx
@@ -10,7 +10,7 @@ interface ThubnailProps {
  * @returns
  */
 const Thumbnail = ({ src, alt }: ThubnailProps) => {
-  return <img src={src} alt={alt} className="h-[11.125rem] w-[6.375rem] bg-gray-200"></img>;
+  return <img src={src} alt={alt} className="h-[11.125rem] w-[6.375rem]"></img>;
 };
 
 export default Thumbnail;
