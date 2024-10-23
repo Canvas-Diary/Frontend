5de56a2481ab5b97ab5f820953ffb501de671a99
leve68 <hanstom000608@gmail.com>
2024-10-23 22:46:17 +0900

    fix: storybook 수정

--- src/components/pages/album/ThumbnailGrid.stories.tsx
+++ src/components/pages/album/ThumbnailGrid.stories.tsx
@@ -1,15 +1,31 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { MemoryRouter } from "react-router-dom";
+import dummy from "../../../assets/dummy/_Image.png";
 
 import ThumbnailGrid from "./ThumbnailGrid";
 
 const meta: Meta<typeof ThumbnailGrid> = {
   component: ThumbnailGrid,
   tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
 };
 
 export default meta;
 type Story = StoryObj<typeof ThumbnailGrid>;
 
 export const Primary: Story = {
-  args: {},
+  args: {
+    diaries: [
+      {
+        diaryId: 1,
+        mainImgUrl: dummy,
+      },
+    ],
+  },
 };
