8ead2ce93674f7ec06852b51ef4255198c9cfe4f
leve68 <hanstom000608@gmail.com>
2024-10-25 15:50:14 +0900

    feat: Appbar 버튼 조건부 제어

--- src/components/common/Appbar.tsx
+++ src/components/common/Appbar.tsx
@@ -3,8 +3,8 @@ import MenuIcon from "../../assets/svg/menu-dots.svg?react";
 
 interface AppBarProps {
   text?: string;
-  backHandler: () => void;
-  menuHandler: () => void;
+  backHandler?: () => void;
+  menuHandler?: () => void;
 }
 
 /**
@@ -17,13 +17,17 @@ interface AppBarProps {
 const Appbar = ({ text, backHandler, menuHandler }: AppBarProps) => {
   return (
     <div className="absolute z-50 flex w-full items-center justify-between bg-transparent px-800 py-300 text-black dark:text-white">
-      <button className="h-7 w-8" onClick={backHandler}>
-        <BackIcon />
-      </button>
+      {backHandler && (
+        <button className="h-7 w-8" onClick={backHandler}>
+          <BackIcon />
+        </button>
+      )}
       {text && <span className="font-Binggrae text-body-1 font-regular">{text}</span>}
-      <button className="h-7 w-8" onClick={menuHandler}>
-        <MenuIcon />
-      </button>
+      {menuHandler && (
+        <button className="h-7 w-8" onClick={menuHandler}>
+          <MenuIcon />
+        </button>
+      )}
     </div>
   );
 };
