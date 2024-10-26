10e9064d387514362529e48fb3347ada5e005265
leve68 <hanstom000608@gmail.com>
2024-10-27 00:42:27 +0900

    feat: Style Option Grid Component

---
+++ src/components/pages/write/StyleOptionGrid.tsx
@@ -0,0 +1,39 @@
+import StyleOption from "./StyleOption";
+import dummy from "../../../assets/dummy/_Image.png";
+import { useState } from "react";
+
+const styles = [
+  { id: 1, image: dummy, text: "수채화1" },
+  { id: 2, image: dummy, text: "수채화2" },
+  { id: 3, image: dummy, text: "수채화3" },
+  { id: 4, image: dummy, text: "수채화4" },
+  { id: 5, image: dummy, text: "수채화5" },
+];
+
+interface StyleOptionGridProps {
+  onClickStyle: Function;
+}
+
+const StyleOptionGrid = ({ onClickStyle }: StyleOptionGridProps) => {
+  const [selectedStyle, setSelectedStyle] = useState<string | null>(null);
+
+  const handleClick = (styleText: string) => {
+    setSelectedStyle(styleText);
+    onClickStyle(styleText);
+  };
+
+  return (
+    <div className="grid grid-cols-3 place-items-center gap-900 overflow-scroll">
+      {styles.map((style) => (
+        <StyleOption
+          key={style.id}
+          image={style.image}
+          text={style.text}
+          isSelected={style.text === selectedStyle}
+          onClick={() => handleClick(style.text)}
+        />
+      ))}
+    </div>
+  );
+};
+export default StyleOptionGrid;
