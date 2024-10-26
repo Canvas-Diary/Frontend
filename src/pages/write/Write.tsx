8f9682b3d3e4d0137f6a1129887ef2400df633e8
leve68 <hanstom000608@gmail.com>
2024-10-27 00:41:31 +0900

    feat: 일기 작성 페이지

--- src/pages/write/Write.tsx
+++ src/pages/write/Write.tsx
@@ -1,5 +1,39 @@
+import { useLocation, useOutletContext } from "react-router-dom";
+import { formatDate } from "../../utils/util";
+import { ContextProps } from "./Layout/DiaryLayout";
+
 const Write = () => {
-  return <div></div>;
+  const location = useLocation();
+  const date = location.state.date;
+  const { diaryInfo, setDiaryInfo } = useOutletContext<ContextProps>();
+
+  return (
+    <div className="flex h-full flex-col gap-600 font-Binggrae text-gray-900">
+      <div className="font-BinggraeBold text-title-2">{formatDate(date)}</div>
+      <div className="flex items-center gap-300 text-body-2">
+        <div className="font-Binggrae text-gray-500">공개 여부</div>
+        <div className="flex items-center justify-center rounded-50 bg-primary-light-2 px-300 py-200">
+          {diaryInfo.isPublic ? "공개" : "비공개"}
+        </div>
+        <button
+          className="ml-auto"
+          onClick={() => {
+            setDiaryInfo({ ...diaryInfo, isPublic: !diaryInfo.isPublic });
+          }}
+        >
+          토글
+        </button>
+      </div>
+      <hr className="w-full border border-gray-100" />
+      <textarea
+        className="flex-grow font-Binggrae text-body-2"
+        value={diaryInfo.diary}
+        onChange={(e) => {
+          setDiaryInfo({ ...diaryInfo, diary: e.target.value });
+        }}
+      ></textarea>
+    </div>
+  );
 };
 
 export default Write;
