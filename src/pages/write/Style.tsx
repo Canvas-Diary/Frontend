9ba17b5877c26712cf8a8ed0fd717fc278935402
leve68 <hanstom000608@gmail.com>
2024-10-27 00:42:37 +0900

    feat: 화풍 선택 화면

--- src/pages/write/Style.tsx
+++ src/pages/write/Style.tsx
@@ -1,5 +1,22 @@
+import { useOutletContext } from "react-router-dom";
+import StyleOptionGrid from "../../components/pages/write/StyleOptionGrid";
+import { ContextProps } from "./Layout/DiaryLayout";
+
 const Style = () => {
-  return <div></div>;
+  const { diaryInfo, setDiaryInfo } = useOutletContext<ContextProps>();
+
+  return (
+    <div className="flex h-full flex-col gap-[2rem] font-Binggrae text-gray-900">
+      <div className="font-BinggraeBold text-title-2">
+        <span className="text-primary-medium">화풍</span>을 선택해 주세요
+      </div>
+      <StyleOptionGrid
+        onClickStyle={(style: string) => {
+          setDiaryInfo({ ...diaryInfo, style: style });
+        }}
+      />
+    </div>
+  );
 };
 
 export default Style;
