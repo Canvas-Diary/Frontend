978ea56693cbdd480d8fc71b9f9cfee0d4ee5060
leve68 <hanstom000608@gmail.com>
2024-10-27 16:13:22 +0900

    feat: tailwind 가독성을 위해 배열로 관리

---
+++ src/styles/animations.tsx
@@ -0,0 +1,10 @@
+export const FADEINANIMATION = [
+  "animate-fadeInSlideUp",
+  "animate-fadeInSlideUp ani-delay-100",
+  "animate-fadeInSlideUp ani-delay-200",
+  "animate-fadeInSlideUp ani-delay-300",
+  "animate-fadeInSlideUp ani-delay-400",
+  "animate-fadeInSlideUp ani-delay-500",
+  "animate-fadeInSlideUp ani-delay-600",
+  "animate-fadeInSlideUp ani-delay-700",
+];
