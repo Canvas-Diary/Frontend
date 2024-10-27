a3953e486c1a3cd2c1d22882682098f7aaf45825
leve68 <hanstom000608@gmail.com>
2024-10-27 16:13:59 +0900

    feat: 일기 작성 마지막 페이지

--- src/pages/write/Draw.tsx
+++ src/pages/write/Draw.tsx
@@ -1,5 +1,21 @@
+import dummy from "../../assets/dummy/_Image.png";
+import { FADEINANIMATION } from "../../styles/animations";
+
 const Draw = () => {
-  return <div></div>;
+  return (
+    <div className="items-centergap-[2rem] flex h-full flex-col justify-center font-Binggrae text-gray-900">
+      <div className="flex flex-col items-center justify-center gap-1000 font-BinggraeBold text-title-2">
+        <img
+          src={dummy}
+          alt=""
+          className={`${FADEINANIMATION[0]} h-[9.6rem] w-[9.6rem] rounded-full`}
+        />
+        <div className={`${FADEINANIMATION[1]}`}>
+          <span className="text-primary-medium">그림</span>을 그리고 있어요
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Draw;
