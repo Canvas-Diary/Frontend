30cb9eed94f76842e6080a54c27eda55401e5f45
leve68 <hanstom000608@gmail.com>
2024-10-31 21:04:02 +0900

    feat: 일기 생성, 조회, 달력조회 api

---
+++ src/api/api.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  withCredentials: true,
+  baseURL: "/api",
+});
+
+export const createDiaryAndGetId = async (newDiaryInfo: NewDiaryInfo) => {
+  try {
+    const response = await axiosInstance.post("/api/v1/diaries", newDiaryInfo);
+    return response.data.diaryId;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getDiaryInfoById = async (diaryId: string) => {
+  try {
+    const response = await axiosInstance.get(`/api/v1/diaries/${diaryId}/my`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getMonthlyDiariesByDate = async (date: string) => {
+  try {
+    const params = { date: date };
+    const response = await axiosInstance.get(`/api/v1/diaries`, { params });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export interface NewDiaryInfo {
+  date: Date;
+  content: string;
+  style: string;
+  isPublic: boolean;
+}
