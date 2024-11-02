df54e4f2a00eadcf9b52eaab36c85963de613653
leve68 <hanstom000608@gmail.com>
2024-11-02 11:17:23 +0900

    feat: errorboundary에 맞는 error page

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,9 +1,17 @@
+import { FallbackProps } from "react-error-boundary";
+
 /**
  * 오류 화면
  * @returns
  */
-const Error = () => {
-  return <div>Error</div>;
+const Error = ({ error, resetErrorBoundary }: FallbackProps) => {
+  return (
+    <div>
+      <h1>Something went wrong!</h1>
+      <p>{error.message || "An unexpected error occurred."}</p>
+      <button onClick={resetErrorBoundary}>Try Again</button>
+    </div>
+  );
 };
 
 export default Error;
