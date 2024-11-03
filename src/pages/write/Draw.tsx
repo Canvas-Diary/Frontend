e608d148a52dc3eac0e1333de5c9a8f327b175ed
leve68 <hanstom000608@gmail.com>
2024-11-03 20:34:46 +0900

    feat: 그림 생성 완료되면 애니메이션으로 표시

--- src/pages/write/Draw.tsx
+++ src/pages/write/Draw.tsx
@@ -1,7 +1,11 @@
+import { useOutletContext } from "react-router-dom";
 import dummy from "../../assets/dummy/_Image.png";
 import { FADEINANIMATION } from "../../styles/animations";
+import { ContextProps } from "./Layout/DiaryWriteFlowLayout";
 
 const Draw = () => {
+  const { diaryId } = useOutletContext<ContextProps>();
+
   return (
     <div className="items-centergap-[2rem] flex h-full flex-col justify-center font-Binggrae text-gray-900">
       <div className="flex flex-col items-center justify-center gap-1000 font-BinggraeBold text-title-2">
@@ -10,8 +14,17 @@ const Draw = () => {
           alt=""
           className={`${FADEINANIMATION[0]} h-[9.6rem] w-[9.6rem] rounded-full`}
         />
-        <div className={`${FADEINANIMATION[1]}`}>
-          <span className="text-primary-medium">그림</span>을 그리고 있어요
+        <div className={`${FADEINANIMATION[1]} flex flex-col gap-500 overflow-hidden`}>
+          <div
+            className={`${diaryId !== "0" && "-translate-y-11 opacity-0"} transition-all duration-1000`}
+          >
+            <span className="text-primary-medium">그림</span>을 그리고 있어요
+          </div>
+          <div
+            className={`${diaryId !== "0" ? "-translate-y-11 opacity-100" : "opacity-0"} transition-all duration-1000`}
+          >
+            <span className="text-primary-medium">그림</span>이 다 그려졌어요
+          </div>
         </div>
       </div>
     </div>
