756e869e99b3705ef393757b92d02e3d10a00df0
leve68 <hanstom000608@gmail.com>
2024-11-03 20:36:53 +0900

    fix: 빌드 오류 해결

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -10,6 +10,7 @@ const Error = ({ error, resetErrorBoundary }: FallbackProps) => {
       <h1>Something went wrong!</h1>
       <p>{error.message || "An unexpected error occurred."}</p>
       <button onClick={() => window.location.replace("/")}>홈으로</button>
+      <button onClick={() => resetErrorBoundary()}>에러 초기화</button>
     </div>
   );
 };
