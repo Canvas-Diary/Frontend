7575b32d0681093452ca67d78f925379fdef3ba9
leve68 <hanstom000608@gmail.com>
2024-11-04 10:31:16 +0900

    feat: mypage 레이아웃

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
+import Appbar from "../components/common/Appbar";
+import Divider from "../components/common/Divider";
+import Toggle from "../components/common/Toggle";
+
 /**
  * 마이페이지 화면
  * @returns
  */
 const Mypage = () => {
-  return <div className="flex flex-grow flex-col overflow-scroll"></div>;
+  const [dark, setDark] = useState(false);
+  return (
+    <div className="flex flex-grow flex-col overflow-scroll">
+      <Appbar text="마이페이지"></Appbar>
+      <div className="flex flex-col gap-600 p-800 font-Binggrae text-body-1">
+        <div className="flex">
+          <button className="w-full text-start">다크모드</button>
+          <Toggle
+            onClickHandler={() => {
+              setDark((prev) => !prev);
+            }}
+            isChecked={dark}
+          />
+        </div>
+        <Divider />
+        <button className="w-full text-start">통계</button>
+        <Divider />
+        <button className="w-full text-start text-status-negative">회원탈퇴</button>
+        <Divider />
+        <button className="w-full text-start text-status-negative">로그아웃</button>
+      </div>
+    </div>
+  );
 };
 
 export default Mypage;
