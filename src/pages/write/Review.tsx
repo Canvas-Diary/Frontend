1c33714d7a38f32b3d194489ff79a2a5089a47fa
leve68 <hanstom000608@gmail.com>
2024-11-04 15:33:48 +0900

    feat: review dummy diary 추가

--- src/pages/write/Review.tsx
+++ src/pages/write/Review.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 import ReviewContent from "../../components/pages/write/ReviewContent";
 import { FADEINANIMATION } from "../../styles/animations";
+import { DiaryInfo } from "../../types/types";
+import { getMyDiaryInfoById } from "../../api/api";
+
+const dummyId = "0192f5d7-c29d-5b55-4b74-f1c412201dea";
 
 const Review = () => {
+  const [diaryInfo, setDiaryInfo] = useState<DiaryInfo | null>(null);
+
+  useEffect(() => {
+    const fetchDiary = async () => {
+      try {
+        const data = await getMyDiaryInfoById(dummyId);
+        setDiaryInfo(data);
+      } catch (error) {
+        setDiaryInfo(null);
+      }
+    };
+
+    fetchDiary();
+  }, []);
+
   return (
     <div className="flex h-full flex-col gap-[2rem] font-Binggrae text-gray-900">
       <div className={`${FADEINANIMATION[0]} font-BinggraeBold text-title-2`}>
@@ -9,13 +29,15 @@ const Review = () => {
         비슷한 일기를 작성했어요
       </div>
       <div className={`${FADEINANIMATION[1]} flex-grow pb-[2rem]`}>
-        <ReviewContent
-          date="2024"
-          emotion=""
-          content="일기"
-          likedCount={10}
-          isLiked={false}
-        ></ReviewContent>
+        {diaryInfo && (
+          <ReviewContent
+            date={diaryInfo.date}
+            emotion={diaryInfo.emotion}
+            content={diaryInfo.content}
+            likedCount={diaryInfo.likedCount}
+            isLiked={diaryInfo.isLiked}
+          ></ReviewContent>
+        )}
       </div>
     </div>
   );
