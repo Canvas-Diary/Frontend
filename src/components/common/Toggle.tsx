055d03b608075c98a8decfb7a50a73c2513bd6d7
leve68 <hanstom000608@gmail.com>
2024-11-04 10:31:29 +0900

    fix: Toggle 그림자 추가

--- src/components/common/Toggle.tsx
+++ src/components/common/Toggle.tsx
@@ -18,7 +18,7 @@ const Toggle = ({ onClickHandler, isChecked }: ToggleProps) => {
           handleToggle();
         }}
       />
-      <div className="peer relative h-[1.75rem] w-[3.25rem] rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-6 after:w-6 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-primary-medium peer-checked:after:translate-x-full peer-checked:after:border-white rtl:peer-checked:after:-translate-x-full"></div>
+      <div className="peer relative h-[1.75rem] w-[3.25rem] rounded-full bg-gray-200 shadow-default after:absolute after:start-[2px] after:top-[2.4px] after:h-6 after:w-[1.48rem] after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-primary-medium peer-checked:after:translate-x-full peer-checked:after:border-white rtl:peer-checked:after:-translate-x-full"></div>
     </label>
   );
 };
