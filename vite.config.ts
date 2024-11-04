a27d327c7a6718ef5ce573b3e75506b7003e276e
leve68 <hanstom000608@gmail.com>
2024-11-04 16:06:17 +0900

    build: 빌드 asset 파일 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,27 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+            return "assets/images/[name]-[hash][extname]";
+          }
+
+          if (/\.css$/.test(name ?? "")) {
+            return "assets/css/[name]-[hash][extname]";
+          }
+
+          if (/\.(ttf|otf|woff|woff2|eot)$/.test(name ?? "")) {
+            return "assets/fonts/[name]-[hash][extname]";
+          }
+
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
   // server: {
   //   proxy: {
   //     "/api": {
