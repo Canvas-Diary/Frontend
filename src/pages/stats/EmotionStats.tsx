e0c008163036d5296752a63c76435dd3cea5d383
leve68 <hanstom000608@gmail.com>
2024-11-14 17:50:32 +0900

    feat: tab ui 커스텀, 적용

---
+++ src/pages/stats/EmotionStats.tsx
@@ -0,0 +1,22 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+const EmotionStats = () => {
+  return (
+    <div className="flex w-full flex-col items-center justify-center bg-primary-light-1 px-800 py-500">
+      <Tabs defaultValue="week" className="w-full text-center font-Binggrae">
+        <TabsList className="w-80">
+          <TabsTrigger value="week" className="w-full">
+            1주
+          </TabsTrigger>
+          <TabsTrigger value="month" className="w-full">
+            1달
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="week">Make changes to your account here.</TabsContent>
+        <TabsContent value="month">Change your password here.</TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default EmotionStats;
