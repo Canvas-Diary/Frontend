d53fcde9a45758953d36fded707684e75c770539
leve68 <hanstom000608@gmail.com>
2024-11-17 18:05:00 +0900

    feat: blocker를 이용한 dialog

--- src/pages/write/Layout/DiaryWriteFlowLayout.tsx
+++ src/pages/write/Layout/DiaryWriteFlowLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Outlet, useBlocker, useLocation, useNavigate } from "react-router-dom";
 import Appbar from "../../../components/common/Appbar";
 import Button from "../../../components/common/Button";
 import RoutePaths from "../../../constants/routePath";
@@ -8,6 +8,15 @@ import { useErrorBoundary } from "react-error-boundary";
 import MobileLayout from "../../Layout/MobileLayout";
 import { getTodayDate } from "../../../utils/util";
 import { NewDiaryInfo } from "../../../types/types";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { DialogClose } from "@radix-ui/react-dialog";
 
 const pageOrder = [
   RoutePaths.diaryWrite,
@@ -35,6 +44,7 @@ const DiaryWriteFlowLayout = () => {
   });
 
   const onClickNext = async () => {
+    console.log(diaryInfo);
     const currentIndex = pageOrder.indexOf(location.pathname);
 
     if (currentIndex !== -1 && currentIndex < pageOrder.length - 1) {
@@ -86,6 +96,25 @@ const DiaryWriteFlowLayout = () => {
     return true;
   };
 
+  const blocker = useBlocker(({ currentLocation, nextLocation, historyAction }) => {
+    const isInternalNavigation =
+      currentLocation.pathname.startsWith("/diary/") && nextLocation.pathname.startsWith("/diary/");
+    if (isInternalNavigation) {
+      return false;
+    }
+
+    if (historyAction === "PUSH") {
+      return false;
+    } else if (historyAction === "REPLACE") {
+      // 현재 페이지를 대체하려고 할 때 허용
+      return false;
+    } else if (historyAction === "POP") {
+      // 브라우저의 뒤로 가기 또는 앞으로 가기 버튼을 눌렀을 때 확인 메시지 표시
+      return true;
+    }
+    return false; // 기본적으로 차단하지 않음
+  });
+
   return (
     <MobileLayout>
       <Appbar text="일기 작성" backHandler={handleBack}></Appbar>
@@ -101,6 +130,43 @@ const DiaryWriteFlowLayout = () => {
           bgColor="dark"
         />
       </div>
+
+      <Dialog open={blocker.state === "blocked"}>
+        <DialogContent
+          className="min-w-fit max-w-[95%] rounded-200"
+          onClick={() => {
+            blocker.state === "blocked" && blocker!.reset();
+          }}
+        >
+          <DialogHeader>
+            <DialogTitle>정말 나가시겠습니까?</DialogTitle>
+            <DialogDescription>변경사항이 저장되지 않을 수 있습니다.</DialogDescription>
+          </DialogHeader>
+          <DialogFooter className="flex w-full flex-row">
+            <DialogClose asChild>
+              <Button
+                size="small"
+                active={true}
+                text="남아있기"
+                bgColor="light"
+                onClickHandler={() => {
+                  blocker.state === "blocked" && blocker.reset();
+                }}
+              ></Button>
+            </DialogClose>
+
+            <Button
+              size="small"
+              active={true}
+              text="나가기"
+              bgColor="dark"
+              onClickHandler={() => {
+                blocker.state === "blocked" && blocker.proceed();
+              }}
+            ></Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </MobileLayout>
   );
 };
