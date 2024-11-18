e2d671cdafea4d054ec04afef423cb618d127f5b
leve68 <hanstom000608@gmail.com>
2024-11-18 20:45:44 +0900

    feat: addbutton 다크 모드 적용

--- src/components/pages/diary/AddButton.tsx
+++ src/components/pages/diary/AddButton.tsx
@@ -7,7 +7,7 @@ interface AddButtonProps {
 const AddButton = ({ onClickHandler }: AddButtonProps) => {
   return (
     <button
-      className="flex items-center justify-center gap-400 rounded-full bg-white px-900 py-600 font-Binggrae text-body-2 text-primary-normal shadow-default"
+      className="flex items-center justify-center gap-400 rounded-full bg-white px-900 py-600 font-Binggrae text-body-2 text-primary-normal shadow-default dark:bg-primary-medium dark:text-white"
       onClick={onClickHandler}
     >
       <AddIcon />
