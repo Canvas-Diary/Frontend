5574377b741b725ee9f34be75073116f3dc3088b
leve68 <hanstom000608@gmail.com>
2024-11-23 19:29:10 +0900

    fix: 캐시 삭제 후 커밋

--- src/components/common/divider/Divider.tsx
+++
@@ -1,9 +0,0 @@
-interface DividerProps {
-  style?: string;
-}
-
-const Divider = ({ style }: DividerProps) => {
-  return <hr className={`w-full border border-gray-100 dark:border-gray-800 ${style && style}`} />;
-};
-
-export default Divider;
