c55c527e1badf40da7556662996d85ff149b34dc
leve68 <hanstom000608@gmail.com>
2024-11-24 02:36:29 +0900

    fix: 일기 정보를 query hook으로 가져오는 로직으로 변경

--- src/pages/diary/Diary.tsx
+++ src/pages/diary/Diary.tsx
@@ -1,11 +1,7 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { DiaryInfo } from "../../types/types";
-import { getDiaryInfoById } from "../../api/api";
-import DiaryComponent from "../../components/pages/diary/diary/DiaryComponent";
-import DiaryFallback from "../../components/pages/diary/diary/Fallback/DiaryFallback";
-import useMediaQuery from "../../hooks/useMediaQuery";
-import { isValidDate } from "@/utils/util";
+import useFetchDiary from "@/hooks/query/useGetDiaryInfo";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import DiaryComponent from "@/components/pages/diary/diary/DiaryComponent";
 
 /**
  * 일기 화면 레이아웃
@@ -14,43 +10,16 @@ import { isValidDate } from "@/utils/util";
  */
 const Diary = () => {
   const { diaryId } = useParams<{ diaryId: string }>();
-  const [diaryInfo, setDiaryInfo] = useState<DiaryInfo | null>(null);
-  const [loading, setLoading] = useState(true);
+  const { diaryInfo, refetch } = useFetchDiary(diaryId!);
   const { calculatedHeight } = useMediaQuery();
-  const [isMyDiary, setIsMyDiary] = useState(false);
-
-  const fetchDiary = async () => {
-    if (!diaryId) return;
-    try {
-      const data = await getDiaryInfoById(diaryId);
-      if (data.isMine) setIsMyDiary(true);
-      else setIsMyDiary(false);
-
-      setDiaryInfo(data);
-    } catch (error) {
-      setDiaryInfo(null);
-      throw error;
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    if (!isValidDate(diaryId!)) fetchDiary();
-    else setLoading(false);
-  }, []);
 
   return (
-    <>
-      {loading && <DiaryFallback />}
-
-      <DiaryComponent
-        diaryInfo={diaryInfo!}
-        carouselHeight={calculatedHeight}
-        isMyDiary={isMyDiary}
-        retry={fetchDiary}
-      />
-    </>
+    <DiaryComponent
+      diaryInfo={diaryInfo}
+      carouselHeight={calculatedHeight}
+      isMyDiary={diaryInfo.isMine}
+      retry={refetch}
+    />
   );
 };
 
