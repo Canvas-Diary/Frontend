0231422595d7da14b9973e5e45f295c971758f78
leve68 <hanstom000608@gmail.com>
2024-11-25 16:12:03 +0900

    feat: 회고 없음 Api boundary로 처리

--- src/components/pages/diary/diaryflow/Content/Review.tsx
+++ src/components/pages/diary/diaryflow/Content/Review.tsx
@@ -1,11 +1,12 @@
-import { getReview } from "@/api/api";
 import { FADEINANIMATION } from "@/styles/animations";
-import { FlowDiaryInfo, ReviewDiaryInfo } from "@/types/types";
-import { useEffect, useState } from "react";
+import { FlowDiaryInfo } from "@/types/types";
 import ReviewContent from "../../../write/ReviewContent";
 import { formatDateWithWeek } from "@/utils/util";
 import DiaryFlowLayout from "../Layout";
 import Button from "@/components/common/Button/Button";
+import ApiBoundary from "@/components/common/boundary/ApiBoundary";
+import useGetReviewInfo from "@/hooks/query/useGetReviewInfo";
+import ReviewSkeleton from "../skeleton/ReviewSkeleton";
 
 interface ReviewProps {
   diaryInfo: FlowDiaryInfo;
@@ -13,60 +14,16 @@ interface ReviewProps {
 }
 
 const Review = ({ diaryInfo, onClickNext }: ReviewProps) => {
-  const [reviewDiaryInfo, setReviewDiaryInfo] = useState<ReviewDiaryInfo | null>(null);
-
   const onClickNextHandler = () => {
     onClickNext();
-  };
-
-  useEffect(() => {
-    const fetchDiary = async () => {
-      try {
-        const data = await getReview({ content: diaryInfo.content, date: diaryInfo.date });
-        setReviewDiaryInfo(data);
-      } catch (error) {
-        setReviewDiaryInfo(null);
-      }
-    };
-
-    fetchDiary();
-  }, []);
-
-  const typeText = (type: "YEAR" | "MONTH" | "KEYWORD") => {
-    switch (type) {
-      case "KEYWORD":
-        return "비슷한 내용";
-      case "MONTH":
-        return "한달 전";
-      case "YEAR":
-        return "일년 전";
-    }
   };
 
   return (
     <>
       <DiaryFlowLayout>
-        {reviewDiaryInfo && (
-          <>
-            <div className={`${FADEINANIMATION[0]} font-BinggraeBold text-title-2`}>
-              <span className="text-primary-medium dark:text-primary-light-3">
-                {typeText(reviewDiaryInfo.type)}
-              </span>
-              의<br />
-              일기를 확인해보세요
-            </div>
-            <div className={`${FADEINANIMATION[1]} flex-grow pb-[2rem]`}>
-              <ReviewContent
-                date={formatDateWithWeek(reviewDiaryInfo.date)}
-                emotion={reviewDiaryInfo.emotion}
-                content={reviewDiaryInfo.content}
-                likedCount={reviewDiaryInfo.likedCount}
-                isLiked={reviewDiaryInfo.isLiked}
-                images={reviewDiaryInfo.images}
-              ></ReviewContent>
-            </div>
-          </>
-        )}
+        <ApiBoundary fallback={<ReviewSkeleton />} message="비슷한 일기를 찾지 못했어요...">
+          <ReviewContainer diaryInfo={diaryInfo} />
+        </ApiBoundary>
       </DiaryFlowLayout>
       <Button
         className="mx-auto mb-4"
@@ -81,3 +38,42 @@ const Review = ({ diaryInfo, onClickNext }: ReviewProps) => {
 };
 
 export default Review;
+
+interface ReviewContainerProps {
+  diaryInfo: FlowDiaryInfo;
+}
+
+const ReviewContainer = ({ diaryInfo }: ReviewContainerProps) => {
+  const { reviewDiaryInfo } = useGetReviewInfo(diaryInfo.content, diaryInfo.date);
+  const typeText = (type: "YEAR" | "MONTH" | "KEYWORD") => {
+    switch (type) {
+      case "KEYWORD":
+        return "비슷한 내용";
+      case "MONTH":
+        return "한달 전";
+      case "YEAR":
+        return "일년 전";
+    }
+  };
+  return (
+    <>
+      <div className={`${FADEINANIMATION[0]} font-BinggraeBold text-title-2`}>
+        <span className="text-primary-medium dark:text-primary-light-3">
+          {typeText(reviewDiaryInfo.type)}
+        </span>
+        의<br />
+        일기를 확인해보세요
+      </div>
+      <div className={`${FADEINANIMATION[1]} flex-grow pb-[2rem]`}>
+        <ReviewContent
+          date={formatDateWithWeek(reviewDiaryInfo.date)}
+          emotion={reviewDiaryInfo.emotion}
+          content={reviewDiaryInfo.content}
+          likedCount={reviewDiaryInfo.likedCount}
+          isLiked={reviewDiaryInfo.isLiked}
+          images={reviewDiaryInfo.images}
+        ></ReviewContent>
+      </div>
+    </>
+  );
+};
