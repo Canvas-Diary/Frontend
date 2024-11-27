791c36cc3eacecafd9fb00d056b7ab399bd3df02
leve68 <hanstom000608@gmail.com>
2024-11-27 23:27:21 +0900

    feat: 온보딩 애니메이션 추가

--- src/pages/user/Onboarding.tsx
+++ src/pages/user/Onboarding.tsx
@@ -6,6 +6,8 @@ import img1 from "@/assets/images/onboard1.png";
 import img2 from "@/assets/images/onboard2.png";
 import img3 from "@/assets/images/onboard3.png";
 import img4 from "@/assets/images/onboard4.png";
+import useInView from "@/hooks/useInView";
+import { FADEINANIMATION } from "@/styles/animations";
 
 interface OnboardingProps {
   onClose: () => void;
@@ -13,6 +15,10 @@ interface OnboardingProps {
 
 const Onboarding = ({ onClose }: OnboardingProps) => {
   const [api, setApi] = useState<CarouselApi>();
+  const { elementRef: ref1, isInView: inView1 } = useInView<HTMLDivElement>(0.7, () => {});
+  const { elementRef: ref2, isInView: inView2 } = useInView<HTMLDivElement>(0.7, () => {});
+  const { elementRef: ref3, isInView: inView3 } = useInView<HTMLDivElement>(0.7, () => {});
+  const { elementRef: ref4, isInView: inView4 } = useInView<HTMLDivElement>(0.7, () => {});
   const [current, setCurrent] = useState(0);
 
   const handleSelect = () => {
@@ -45,37 +51,66 @@ const Onboarding = ({ onClose }: OnboardingProps) => {
       </ol>
       <Carousel className="h-full" setApi={setApi}>
         <CarouselContent className="h-full font-Binggrae text-heading-1">
-          <CarouselItem key={0} className="flex h-full w-full flex-col items-center justify-center">
+          <CarouselItem
+            ref={ref1}
+            key={0}
+            className="flex h-full w-full flex-col items-center justify-center"
+          >
             <div className="my-auto flex w-full flex-col items-center">
-              <p>날짜를 클릭해</p>
-              <p className="text-primary-normal">일기를 작성하거나 확인하세요</p>
+              <p className={`${inView1 && FADEINANIMATION[0]} opacity-0`}>날짜를 클릭해</p>
+              <p className={`${inView1 && FADEINANIMATION[1]} text-primary-normal opacity-0`}>
+                일기를 작성하거나 확인하세요
+              </p>
             </div>
-            <img src={img1} alt="" />
+            <img src={img1} alt="" className={`${inView1 && FADEINANIMATION[2]} opacity-0`} />
           </CarouselItem>
-          <CarouselItem key={1} className="flex h-full w-full flex-col items-center justify-center">
+
+          <CarouselItem
+            ref={ref2}
+            key={1}
+            className="flex h-full w-full animate-fadeInSlideUp flex-col items-center justify-center"
+          >
             <div className="my-auto flex w-full flex-col items-center">
-              <p>
+              <p className={`${inView2 && FADEINANIMATION[0]} opacity-0`}>
                 일기를 작성하면 AI가 <span className="text-primary-normal">감정을</span>
               </p>
-              <p className="text-primary-normal">분석하고 관련 이미지를 생성합니다</p>
+              <p className={`${inView2 && FADEINANIMATION[1]} text-primary-normal opacity-0`}>
+                분석하고 관련 이미지를 생성합니다
+              </p>
             </div>
-            <img src={img2} alt="" />
+            <img src={img2} alt="" className={`${inView2 && FADEINANIMATION[2]} opacity-0`} />
           </CarouselItem>
-          <CarouselItem key={2} className="flex h-full w-full flex-col items-center justify-center">
+
+          <CarouselItem
+            ref={ref3}
+            key={2}
+            className="flex h-full w-full animate-fadeInSlideUp flex-col items-center justify-center"
+          >
             <div className="my-auto flex w-full flex-col items-center">
-              <p>다른 사람들의 일기를 통해</p>
-              <p className="text-primary-normal">새로운 이야기를 만나보세요</p>
+              <p className={`${inView3 && FADEINANIMATION[0]} opacity-0`}>
+                다른 사람들의 일기를 통해
+              </p>
+              <p className={`${inView3 && FADEINANIMATION[1]} text-primary-normal opacity-0`}>
+                새로운 이야기를 만나보세요
+              </p>
             </div>
-            <img src={img3} alt="" />
+            <img src={img3} alt="" className={`${inView3 && FADEINANIMATION[2]} opacity-0`} />
           </CarouselItem>
-          <CarouselItem key={3} className="flex h-full w-full flex-col items-center justify-center">
+
+          <CarouselItem
+            ref={ref4}
+            key={3}
+            className="flex h-full w-full animate-fadeInSlideUp flex-col items-center justify-center"
+          >
             <div className="my-auto flex w-full flex-col items-center">
-              <p className="text-primary-normal">내 일기의 감정과 키워드를 분석한</p>
-              <p>
+              <p className={`${inView4 && FADEINANIMATION[0]} opacity-0`}>
+                내 일기의 감정과 키워드를 분석한
+              </p>
+              <p className={`${inView4 && FADEINANIMATION[1]} opacity-0`}>
                 <span className="text-primary-normal">통계</span>를 확인하세요
               </p>
             </div>
-            <img src={img4} alt="" />
+            <img src={img4} alt="" className={`${inView4 && FADEINANIMATION[2]} opacity-0`} />
           </CarouselItem>
         </CarouselContent>
       </Carousel>
