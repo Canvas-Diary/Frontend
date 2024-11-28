2cca163f6fe2279bfa026cd2a783d21d6fa8c07c
leve68 <hanstom000608@gmail.com>
2024-11-29 00:29:02 +0900

    fix: 한국 표준시 적용, 월 변경시 오늘일기작성 버튼 로직 수정

--- src/hooks/query/useCalendarData.tsx
+++ src/hooks/query/useCalendarData.tsx
@@ -1,9 +1,11 @@
 import { getMonthlyDiariesByDate } from "@/api/api";
 import { formatDate } from "@/utils/util";
 import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 
 const useCalendarData = (currentDate: Date) => {
   const formattedDate = formatDate(currentDate);
+  const [isActiveToday, setIsActiveToday] = useState(true);
 
   const { data: calendarData } = useQuery({
     queryKey: ["calendarData", formattedDate],
@@ -11,8 +13,23 @@ const useCalendarData = (currentDate: Date) => {
     staleTime: 1000 * 60 * 5,
   });
 
-  const todayString = new Date().toISOString().split("T")[0];
-  const isActiveToday = !calendarData?.diaries.some((diary) => diary.date === todayString);
+  const todayString = new Intl.DateTimeFormat("ko-KR", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  })
+    .format(new Date())
+    .replace(/\. /g, "-")
+    .replace(".", "");
+
+  useEffect(() => {
+    if (calendarData) {
+      const hasTodayDiary = calendarData.diaries.some((diary) => diary.date === todayString);
+
+      // 오늘 날짜의 상태를 유지하기 위한 조건
+      setIsActiveToday((prev: boolean) => prev && !hasTodayDiary);
+    }
+  }, [calendarData, todayString]);
 
   return { calendarData, isActiveToday };
 };
